591532690799ccd3bcbbb831a8a3bc01031257f6
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-11-06 21:29:02 +0700

    v1

--- app/src/main/java/codes/justsource/assignment2/MainActivity.java
+++ app/src/main/java/codes/justsource/assignment2/MainActivity.java
@@ -1,44 +1,85 @@
 package codes.justsource.assignment2;
 
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements SensorListener{
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private static Context mContext;
+
+    /* Animation */
+    private static AnimationDrawable mAnimation = new AnimationDrawable();
+    private ImageView img;
+
+    /* Sensor */
     private static final int SHAKE_THRESHOLD = 800;
     private int lastUpdate;
+    private static SensorInfo sensorInfo = new SensorInfo();;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+
+    final int POLL_INTERVAL = 500; //ms
+    private Handler handler = new Handler();
+    private Runnable pollTask = new Runnable() {
+        @Override
+        public void run() {
+            sensorInfo.checkZiamZee(mAnimation, mContext);
+            handler.postDelayed(pollTask, POLL_INTERVAL);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
 
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorManager.registerListener((SensorListener) this,
-                SensorManager.SENSOR_ACCELEROMETER,
-                SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    @Override
-    public void onSensorChanged(int sensor, float[] values) {
-        if(sensor == SensorManager.SENSOR_ACCELEROMETER) {
-            long curTime = System.currentTimeMillis();
-            //only allow one update every 100ms.
-            if((curTime - lastUpdate) > 100) {
-                long diffTime = (curTime - lastUpdate);
-//                lastUpdate = curTime;
-//
-//                x = values[SensorManager.DATA_X];
-            }
+        /* Animation */
+        final int resonableDuration = 50; //ms
+        img = (ImageView) findViewById(R.id.img);
+        BitmapDrawable[] frames = new BitmapDrawable[4];
+        for(int i = 0; i < 4; i++) {
+            frames[i] = (BitmapDrawable) getResources().getDrawable(getResources().getIdentifier("frame_" + i, "drawable", this.getPackageName()));
+            mAnimation.addFrame(frames[i], resonableDuration);
         }
+        img.setImageDrawable(mAnimation);
+
+//        mAnimation.start();
     }
 
     @Override
-    public void onAccuracyChanged(int sensor, int accuracy) {
+    protected void onResume() {
+        super.onResume();
+
+        /* Sensor */
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
+        handler.postDelayed(pollTask, POLL_INTERVAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float xAcc = sensorEvent.values[0];
+        float yAcc = sensorEvent.values[1];
+        float zAcc = sensorEvent.values[2];
+
+        sensorInfo.setAcc(xAcc, yAcc, zAcc);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
--- app/src/main/java/codes/justsource/assignment2/SensorInfo.java
+++ app/src/main/java/codes/justsource/assignment2/SensorInfo.java
@@ -1,21 +1,26 @@
 package codes.justsource.assignment2;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.AnimationDrawable;
+import android.util.Log;
+
 public class SensorInfo {
     private float accX, accY, accZ;
-    private int shakeThreshold;
+    private final int SHAKE_THRESHOLD = 15;
+
+    private boolean isShowingDialog;
+    /* Dialog */
 
     public SensorInfo() {
         accX = 0;
         accY = 0;
         accZ = 0;
-        shakeThreshold = 0;
+        isShowingDialog = false;
     }
 
-    public void setShakeThreshold(int shakeThreshold) {
-        this.shakeThreshold = shakeThreshold;
-    }
-
-    public void setAcc(int accX, int accY, int accZ) {
+    public void setAcc(float accX, float accY, float accZ) {
         this.accX = accX;
         this.accY = accY;
         this.accZ = accZ;
@@ -23,6 +28,35 @@ public class SensorInfo {
 
     public boolean isShake() {
         return false;
+    }
+
+    public void checkZiamZee(final AnimationDrawable mAnimation, Context context) {
+
+        //isShaking right now
+        if(Math.abs(accX) > SHAKE_THRESHOLD || Math.abs(accY) > SHAKE_THRESHOLD || Math.abs(accZ) > SHAKE_THRESHOLD ) {
+            if(!isShowingDialog) {
+                isShowingDialog = true;
+
+                mAnimation.start();
+
+                /* Random Ziamzee */
+                final int ziamzeeIndex = (int) Math.round( Math.random()* (Ziamzee.getZiamzeeLength()-1) );
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+                dialog.setTitle("Your ziamzee is number: "+ (ziamzeeIndex+1));
+                dialog.setIcon(android.R.drawable.btn_star_big_on);
+                dialog.setMessage(Ziamzee.getZiamzee(ziamzeeIndex));
+                dialog.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        isShowingDialog = false;
+                        mAnimation.stop();
+                    }
+                });
+                dialog.show();
+            }
+        }
     }
 
 }
